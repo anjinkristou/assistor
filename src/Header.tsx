3b680ca8541a9eb5212e6d4e6a62ac328273be61
Mehrez Kristou <anjin.kristou@gmail.com>
2021-11-08 16:14:03 +0100

    Drawer layout

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import { Tabs, Tab, Toolbar, AppBar, Box, Typography } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
 import { Link, useRouteMatch } from 'react-router-dom';
 import { UserMenu, Logout, LoadingIndicator } from 'react-admin';
 import LiveHelp from '@material-ui/icons/LiveHelp';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MenuIcon from '@material-ui/icons/Menu';
+import Badge from '@material-ui/core/Badge';
 
+const drawerWidth = 240;
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
@@ -14,63 +20,65 @@ const useStyles = makeStyles({
         width: 50,
         height: 43.54,
     },
-});
+    toolbar: {
+        paddingRight: 24, // keep right padding when drawer closed
+    },
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    menuButtonHidden: {
+        display: 'none',
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
-const Header = () => {
+const Header = ({open, setOpen}: any) => {
     const classes = useStyles();
     const match = useRouteMatch(['/contacts', '/companies', '/deals']);
     const currentPath = match?.path ?? '/';
 
     return (
-        <nav className={classes.root}>
-            <AppBar position="static" color="primary">
-                <Toolbar variant="dense">
-                    <Box flex={1} display="flex" justifyContent="space-between">
-                        <Box display="flex" alignItems="center" gridGap={2}>
-                            <LiveHelp />
-                            <Typography component="span" variant="h5">
-                                Assistor
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <Tabs
-                                value={currentPath}
-                                aria-label="Navigation Tabs"
-                            >
-                                <Tab
-                                    label={'Dashboard'}
-                                    component={Link}
-                                    to="/"
-                                    value="/"
-                                />
-                                <Tab
-                                    label={'Contacts'}
-                                    component={Link}
-                                    to="/contacts"
-                                    value="/contacts"
-                                />
-                                <Tab
-                                    label={'Companies'}
-                                    component={Link}
-                                    to="/companies"
-                                    value="/companies"
-                                />
-                                <Tab
-                                    label={'Deals'}
-                                    component={Link}
-                                    to="/deals"
-                                    value="/deals"
-                                />
-                            </Tabs>
-                        </Box>
-                        <Box display="flex">
-                            <LoadingIndicator />
-                            <UserMenu logout={<Logout button />} />
-                        </Box>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </nav>
+        <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+            <Toolbar className={classes.toolbar}>
+            <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={() => setOpen(true)}
+                className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                Assistor
+            </Typography>
+            <UserMenu logout={<Logout button />} />
+            </Toolbar>
+        </AppBar>
     );
 };
 
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { createTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import { CssBaseline, Container } from '@material-ui/core';
+import { CssBaseline, Container, Box } from '@material-ui/core';
 import { CoreLayoutProps } from 'react-admin';
 
 import { Notification, Error } from 'react-admin';
 import Header from './Header';
+import LeftDrawer from './LeftDrawer';
 
 class Layout extends Component<LayoutProps, LayoutState> {
     state: LayoutState = {
         hasError: false,
         errorMessage: undefined,
         errorInfo: undefined,
+        drawerOpen: false,
     };
 
     constructor(props: any) {
@@ -41,25 +43,32 @@ class Layout extends Component<LayoutProps, LayoutState> {
     render() {
         const { theme, title, children } = this.props;
         const { hasError, errorMessage, errorInfo } = this.state;
+
         return (
             // @ts-ignore
             <ThemeProvider theme={createTheme(theme)}>
-                <CssBaseline />
-                <Header />
-                <Container>
-                    <main id="main-content">
-                        {hasError ? (
-                            <Error
-                                error={errorMessage as Error}
-                                errorInfo={errorInfo}
-                                title={title as string}
-                            />
-                        ) : (
-                            children
-                        )}
-                    </main>
-                </Container>
-                <Notification />
+                <Box display="flex">
+                    <CssBaseline />
+                    <Header open={this.state.drawerOpen} setOpen={(state: boolean) => this.setState({drawerOpen: state})}/>
+                    <LeftDrawer open={this.state.drawerOpen} setOpen={(state: boolean) => this.setState({drawerOpen: state})}/>
+                    <Box flexGrow={1} height="100vh" overflow="auto">
+                        <Box sx={{...theme?.mixins?.toolbar}} />
+                        <Container maxWidth="lg">
+                            <main id="main-content">
+                                {hasError ? (
+                                    <Error
+                                        error={errorMessage as Error}
+                                        errorInfo={errorInfo}
+                                        title={title as string}
+                                    />
+                                ) : (
+                                    children
+                                )}
+                            </main>
+                        </Container>
+                        <Notification />
+                    </Box>
+                </Box>
             </ThemeProvider>
         );
     }
@@ -79,6 +88,7 @@ export interface LayoutState {
     hasError: boolean;
     errorMessage?: Error;
     errorInfo?: ErrorInfo;
+    drawerOpen: boolean;
 }
 
 // @ts-ignore
