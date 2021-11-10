d56057fd62f371b1c3ff3f01907ebea3f5a0b381
Mehrez Kristou <anjin.kristou@gmail.com>
2021-11-10 14:38:05 +0100

    Improved token refresh

--- src/authProvider.ts
+++ src/authProvider.ts
@@ -29,6 +29,23 @@ interface UserIdentity {
     avatar: string;
 }
 
+const refreshToken = async () => {
+    const credentials = getCredentials();
+    if(!credentials) return;
+
+    const token = credentials?.refresh_token;
+
+    const config = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+    let response = await axios.post<RefreshToken>(`${baseURL}/refresh`, undefined, config)
+    const { access_token } = response.data;
+    setCredentials({
+        ...credentials,
+        access_token: access_token,
+    });
+}
+
 const login = async ({ username, password }: {username: string; password:string;}) => {
     try{
         const response = await axios.post<LoginToken>(`${baseURL}/login`, { username, password })
@@ -48,6 +65,7 @@ const login = async ({ username, password }: {username: string; password:string;
 
 const getIdentity = async () => { 
     try{
+        refreshToken();
         const credentials = getCredentials();
         const token = credentials?.access_token;
         const config = {
@@ -58,28 +76,18 @@ const getIdentity = async () => {
         
         return Promise.resolve({ id, fullName, avatar });
     } catch (error: any) {
-        const response = error.response;
-        removeCredentials();
-        return Promise.reject({message: response.data, status: response.status});
+        // const response = error.response;
+        // removeCredentials();
+        // return Promise.reject({message: response.data, status: response.status});
+
+        return Promise.reject(error);
     }
  };
 
  const checkError = async ({ status }: any) => {
     if (status === 401 || status === 403 || status === 422) {
-        const credentials = getCredentials();
-        if(!credentials) return Promise.reject();
-
-        const token = credentials?.refresh_token;
         try{
-            const config = {
-                headers: { Authorization: `Bearer ${token}` }
-            };
-            let response = await axios.post<RefreshToken>(`${baseURL}/refresh`, undefined, config)
-            const { access_token } = response.data;
-            setCredentials({
-                ...credentials,
-                access_token: access_token,
-            });
+            refreshToken();
         } catch (error: any) {
             return Promise.reject();
         }
