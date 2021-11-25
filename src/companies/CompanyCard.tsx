a2ca1082d642dd3404f3831a6f5b42a3b636100c
Mehrez Kristou <anjin.kristou@gmail.com>
2021-11-25 16:50:51 +0100

    improved grid size

--- src/companies/CompanyCard.tsx
+++ src/companies/CompanyCard.tsx
@@ -16,7 +16,7 @@ import { CountryField } from './CountryField';
 const useStyles = makeStyles(theme => ({
     paper: {
         height: 200,
-        width: 193.5,
+        width: 185,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
