ca0874e4202f84ea8a8da91f8bd408b4ae1d8a14
Mehrez Kristou <mehrez@kristou.com>
2022-01-04 18:05:26 +0100

    added service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from "./serviceWorker";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -16,3 +17,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+registerServiceWorker();
