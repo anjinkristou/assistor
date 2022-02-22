1c964cccefbf3c60984ab25977bf9196df5b62e4
Mehrez Kristou <mehrez@kristou.com>
2022-02-22 17:36:54 +0100

    Global search started to work

--- src/layout/GlobalSearch.tsx
+++ src/layout/GlobalSearch.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import InputBase from '@material-ui/core/InputBase';
 import { createStyles, alpha, Theme, makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
+import { LinearProgress, Loading, useGetList, useRedirect } from 'react-admin';
+import { Avatar, CircularProgress, List, ListItem, ListItemAvatar, ListItemText, Menu, MenuItem, Popover, Popper } from '@material-ui/core';
+import { GlobalHotKeys } from 'react-hotkeys';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -49,11 +52,49 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const GlobalSearch = () => {
     const classes = useStyles();
+    const [searchTerm, setSearchTerm] = useState("");
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const redirect = useRedirect();
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setAnchorEl(event.currentTarget);
+        setSearchTerm(event.target.value);
+    };
+    
+    const { data, ids, loading, error } = useGetList(
+        'companies',
+        { page: 1, perPage: 10 },
+        { field: 'name', order: 'ASC' },
+        { q: searchTerm },
+        { enabled: searchTerm.length > 0 }
+    );
+    
+    // if (loading) { return <Loading />; }
+    if (error) { return <p>ERROR</p>; }
+    
+    const handleSearchClick = (id: any) => {
+        setAnchorEl(null);
+
+        redirect(`/companies/${id}/show`);
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const keyMap = { SEARCH_HOTKEYS: "command+f" };
+    const handlers = { SEARCH_HOTKEYS: () => console.log("test") };
 
     return (
+      <>
+        <GlobalHotKeys keyMap={keyMap} handlers={handlers} />;
         <div className={classes.search}>
             <div className={classes.searchIcon}>
-              <SearchIcon />
+              {loading 
+                ? <CircularProgress size={24}/>
+                : <SearchIcon />
+              }
+              
             </div>
             <InputBase
               placeholder="Search…"
@@ -62,8 +103,34 @@ const GlobalSearch = () => {
                 input: classes.inputInput,
               }}
               inputProps={{ 'aria-label': 'search' }}
+              autoFocus
+              onChange={handleChange}
             />
           </div>
+          <Menu
+            open={!loading && Boolean(anchorEl) && (ids.length > 0) && (searchTerm.length > 0)}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+              {ids.map((id) => (
+                <MenuItem key={id} onClick={() => handleSearchClick(id)}>
+                  <ListItemAvatar>
+                    <Avatar>
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={data[id].name} />
+                </MenuItem>
+              ))}
+          </Menu>
+      </>
     );
 };
 
